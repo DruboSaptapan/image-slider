c510b60691fdcf3e02b5a1cefed6c44b9a7872ff
DruboSaptapan <saptapan2017@gmail.com>
2021-09-14 09:01:45 +0600

    image-updated

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 const images = [
-    'images/pexels-pixabay-01.jpg',
-    'images/pexels-pixabay-02.jpg',
-    'images/pexels-pixabay-03.jpg',
-    'images/pexels-pixabay-04.jpg',
-    'images/pexels-pixabay-05.jpg',
-    'images/pexels-pixabay-06.jpg',
-    'images/pexels-pixabay-07.jpg',
-    'images/pexels-pixabay-08.jpg',
-    'images/pexels-pixabay-09.jpg',
-    'images/pexels-pixabay-10.jpg',
-    'images/pexels-pixabay-11.jpg',
-    'images/pexels-pixabay-12.jpg',
+    '../images/pexels-pixabay-01.jpg',
+    '../images/pexels-pixabay-02.jpg',
+    '../images/pexels-pixabay-03.jpg',
+    '../images/pexels-pixabay-04.jpg',
+    '../images/pexels-pixabay-05.jpg',
+    '../images/pexels-pixabay-06.jpg',
+    '../images/pexels-pixabay-07.jpg',
+    '../images/pexels-pixabay-08.jpg',
+    '../images/pexels-pixabay-09.jpg',
+    '../images/pexels-pixabay-10.jpg',
+    '../images/pexels-pixabay-11.jpg',
+    '../images/pexels-pixabay-12.jpg',
 ]
 
 let imgIndex = 0;
